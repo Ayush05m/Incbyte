d5d7db6e9fbe6a332df8c46e64fbfbaeb7443bb9
[dyad] <git@dyad.sh>
2025-09-26 13:46:48 +0530

    [dyad] Fixed the invisible price slider handle - wrote 1 file(s)

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,7 +1,9 @@
-import * as React from "react";
-import * as SliderPrimitive from "@radix-ui/react-slider";
+"use client"
 
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import * as SliderPrimitive from "@radix-ui/react-slider"
+
+import { cn } from "@/lib/utils"
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -11,16 +13,20 @@ const Slider = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full touch-none select-none items-center",
-      className,
+      className
     )}
     {...props}
   >
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
+    {/* 
+      Radix UI Slider automatically renders a thumb for each value in the `value` array.
+      We only need to provide the component for one thumb, and it will be used as a template.
+    */}
     <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
-));
-Slider.displayName = SliderPrimitive.Root.displayName;
+))
+Slider.displayName = SliderPrimitive.Root.displayName
 
-export { Slider };
+export { Slider }
